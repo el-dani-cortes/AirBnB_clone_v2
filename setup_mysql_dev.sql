bb3efc236f123a6b25251afa24ab82362ec570fc
Andres Lopez <profesor.de.ingles@aol.com>
2021-03-28 00:05:45 +0000

    ADDED | Updated comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 -- Sets password for user 'hbnb_dev'
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
--- Grants privileges to user on database
+-- Grants privileges to 'hbnb_dev' on db
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- Grants select privileges to user on performance_schema database
+-- Grants select privileges to 'hbnb_dev' on performance_schema db
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
