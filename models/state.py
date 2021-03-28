b4dbaf9a859248df0dd3587e9f6aaf806ccbca5b
el-dani-cortes <danicor83@gmail.com>
2021-03-28 18:32:16 +0000

    UPDATED | Model to work with DBStorage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship('City', backref='state', cascade='all, delete')
+    elif getenv("HBNB_TYPE_STORAGE") == "file":
+        cities = self.cities
+
+        @property
+        def cities(self):
+            """Getter the cities for FileStorage
+            """
+            city_list = []
+            for obj in FileStorage.all(City).keys():
+                if obj.state_id == self.id:
+                    city_list = city_list.append[obj]
+            return city_list
