ea82d2c3d04bb975b461a55f06282b6019609826
el-dani-cortes <danicor83@gmail.com>
2021-03-28 18:29:40 +0000

    ADDED | Import DBStorage class to work with database

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+"""
+This module instantiates an object of class
+FileStorage or DBStorage according the HBNB_TYPE_STORAGE
+"""
+from os import getenv
 
 
-storage = FileStorage()
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+elif getenv('HBNB_TYPE_STORAGE') == 'file':
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
