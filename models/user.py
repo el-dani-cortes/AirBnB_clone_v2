f50db40d1ea375dd4564568922835ab5422df180
Andres Lopez <profesor.de.ingles@aol.com>
2021-03-29 00:05:36 +0000

    UPDATED | Removed extra parameters

--- models/user.py
+++ models/user.py
@@ -6,15 +6,14 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """Defines User"""
+
     if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
     else:
         email = ''
         password = ''
