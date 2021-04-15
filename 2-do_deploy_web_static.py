b5e47a96ad6da1b1348aee2c91f68791522c3c05
el-dani-cortes <danicor83@gmail.com>
2021-04-15 17:47:51 +0000

    UPDATED | Added a try and except to validate if the fab code runs well

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,40 +19,39 @@ def do_deploy(archive_path):
     # Validate if the archive path does not exist
     if not os.path.exists(archive_path):
         return False
-    # Get the file_name
-    split_name = archive_path.split('/')
-    file_name = split_name[1]
+    try:
+        # Get the file_name
+        split_name = archive_path.split('/')
+        file_name = split_name[1]
 
-    # Upload the archive to the /tmp/ directory of the web server
-    put(archive_path, "/tmp/")
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
 
-    # Uncompress the archive to the folder
-    # /data/web_static/releases/<archive filename without extension>
-    # on the web server
-    file_path = file_name.split('.')
-    file_path = '/data/web_static/releases/' + file_path[0]
-    run("mkdir -p {}".format(file_path))
-    run("tar -xzf /tmp/{} -C {}".format(file_name, file_path))
+        # Uncompress the archive to the folder
+        # /data/web_static/releases/<archive filename without extension>
+        # on the web server
+        file_path = file_name.split('.')
+        file_path = '/data/web_static/releases/' + file_path[0]
+        run("mkdir -p {}".format(file_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, file_path))
 
-    # Delete the archive from the web server
-    run("rm /tmp/{}".format(file_name))
+        # Delete the archive from the web server
+        run("rm /tmp/{}".format(file_name))
 
-    # Move files to the rigth folder
-    file_path_move = file_path + '/web_static'
-    run("mv {}/* {}".format(file_path_move, file_path))
+        # Move files to the rigth folder
+        file_path_move = file_path + '/web_static'
+        run("mv {}/* {}".format(file_path_move, file_path))
 
-    # Remove path web_static
-    run("rm -rf {}".format(file_path_move))
+        # Remove path web_static
+        run("rm -rf {}".format(file_path_move))
 
-    # Delete the symbolic link /data/web_static/current from the web server
-    run("rm -rf /data/web_static/current")
+        # Delete the symbolic link /data/web_static/current from the web server
+        run("rm -rf /data/web_static/current")
 
-    # Create a new the symbolic link /data/web_static/current on
-    # the web server, linked to the new version of your code
-    # (/data/web_static/releases/<archive filename without extension>)
-    run("ln -s {} /data/web_static/current".format(file_path))
-
-    # # All runs succesfully
-    #     return True
-    # except:
-    #     return False
+        # Create a new the symbolic link /data/web_static/current on
+        # the web server, linked to the new version of your code
+        # (/data/web_static/releases/<archive filename without extension>)
+        run("ln -s {} /data/web_static/current".format(file_path))
+        return True
+    except:
+        return False
